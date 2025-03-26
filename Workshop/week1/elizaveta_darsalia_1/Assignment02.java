014cf13f4f068c974c141200892413f00a38127d
Elizaveta Darsalia <liza.darsalia17@gmail.com>
2025-03-26 11:44:52 +0400

    Update and rename MineAssignment02.java to Assignment02.java

--- Workshop/week1/elizaveta_darsalia_1/Assignment02.java
+++ Workshop/week1/elizaveta_darsalia_1/Assignment02.java
@@ -3,7 +3,7 @@ package Workshop.week1.elizaveta_darsalia_1;
 import Workshop.week1.elizaveta_darsalia_1.packageOne.ClassA;
 import Workshop.week1.elizaveta_darsalia_1.packageTwo.ClassB;
 
-public class MineAssignment02 {
+public class Assignment02 {
     public static void main(String[] args) {
         ClassA objA = new ClassA(); 
         ClassB objB = new ClassB(); 
