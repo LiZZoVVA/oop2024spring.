a851f24d87667a0b6b94f85ed53223273d254498
Elizaveta Darsalia <liza.darsalia17@gmail.com>
2025-03-26 12:17:37 +0400

    Add files via upload

---
+++ Workshop1/Workshop1/week1/elizaveta_darsalia_1/Assignment03.java
@@ -0,0 +1,9 @@
+package Workshop1.week1.elizaveta_darsalia_1;
+
+public class Assignment03 {
+    public static void main(String[] args) {
+       
+        String filePath = "C:/Users/lizad/Documents/hidden.txt";  
+        System.out.println("The path to the hidden.txt file is: " + filePath);
+    }
+}
