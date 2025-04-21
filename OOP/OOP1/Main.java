66e97f2c03d6bfa0222a6f1f20e64be62dab0442
Elizaveta Darsalia <liza.darsalia17@gmail.com>
2025-04-21 19:14:31 +0400

    Add files via upload

---
+++ OOP/OOP1/Main.java
@@ -0,0 +1,12 @@
+package OOP1;
+
+public class Main {
+    public static void main(String[] args) {
+        Human human1 = new Human("Liza", "Darsalia", "Female", 18, 60);
+
+        System.out.println(human1.name + " " + human1.surname + " is beginner in coding");
+
+        human1.lazy();
+        
+    }
+}
